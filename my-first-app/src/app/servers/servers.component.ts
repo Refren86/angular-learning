9ba28b002a262bd490cff280c8e64d1bcb59f8b9
Refren86 <denis1975shvaiber@gmail.com>
2023-11-22 00:42:14 +0100

    learning...

---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers', // our own html tag
+  // selector: '[app-servers]', // can be selected by attribute
+  // selector: '.app-servers', // can be selected by class
+  templateUrl: './servers.component.html',
+  styleUrl: './servers.component.css'
+})
+export class ServersComponent {
+
+}
