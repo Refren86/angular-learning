cb809a23c1f4e25975d7d404012cb2f96d128622
Refren86 <denis1975shvaiber@gmail.com>
2024-01-01 01:01:42 +0200

    created

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root', // our own html tag
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // variables which go to the html template (data binding)
- 
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
 }
