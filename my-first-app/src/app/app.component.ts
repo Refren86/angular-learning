ac5bfd38d2c5c645b3aefec94ec777134e104953
Refren86 <denis1975shvaiber@gmail.com>
2024-01-08 23:30:35 +0200

    reactiveness

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +16,45 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl('', [Validators.required, this.forbiddenNamesValidator.bind(this)]), // 1st - value; 2nd - validation, 3rd - async validators
-        email: new FormControl('', [Validators.required, Validators.email]),
+        // 1st - value; 2nd - validation, 3rd - async validators
+        username: new FormControl('', [
+          Validators.required,
+          this.forbiddenNamesValidator.bind(this),
+        ]),
+        email: new FormControl(
+          '',
+          [Validators.required, Validators.email],
+          this.forbiddenEmailsValidator
+        ),
       }),
       gender: new FormControl(this.genders[0]),
       hobbies: new FormArray([]),
+    });
+
+    // Value change listener
+    // this.signupForm.valueChanges.subscribe((value) => {
+    //   console.log('Changed value: ', value);
+    // });
+    // Status change listener - valid, invalid, pending
+    this.signupForm.statusChanges.subscribe((status) => {
+      console.log('Changed status: ', status);
+    });
+
+    // Setting form values programmatically
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: 'Refren',
+    //     email: 'refren@test.com',
+    //   },
+    //   gender: 'male',
+    //   hobbies: [],
+    // });
+
+    // Setting specific form fields values
+    this.signupForm.patchValue({
+      userData: {
+        email: 'test@test.xiaom',
+      },
     });
   }
 
@@ -42,7 +77,24 @@ export class AppComponent implements OnInit {
     return null; // successful validation
   }
 
+  forbiddenEmailsValidator(
+    control: FormControl
+  ): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, rej) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ emailIsForbidden: true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+
+    return promise;
+  }
+
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 }
