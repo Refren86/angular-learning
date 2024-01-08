5c722ad3f218ad4356ba9325facae584a35424b7
Refren86 <denis1975shvaiber@gmail.com>
2024-01-09 00:43:35 +0200

    donatello

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,11 +1,65 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root', // our own html tag
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  // variables which go to the html template (data binding)
- 
+export class AppComponent implements OnInit {
+  projectStatuses = ['Stable', 'Critical', 'Finished'];
+  invalidProjectName = 'Test';
+
+  projectForm: FormGroup;
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      projectName: new FormControl(
+        '',
+        [
+          Validators.required,
+          // this.forbiddenProjectNameValidator.bind(this),
+        ],
+        this.forbiddenProjectNameValidatorAsync.bind(this)
+      ),
+      email: new FormControl('', [Validators.email, Validators.required]),
+      projectStatus: new FormControl(this.projectStatuses[0]),
+    });
+  }
+
+  // forbiddenProjectNameValidator(control: FormControl): {
+  //   [s: string]: boolean;
+  // } {
+  //   if (control.value.toLowerCase() === this.invalidProjectName.toLowerCase()) {
+  //     return {
+  //       projectNameIsForbidden: true,
+  //     };
+  //   }
+
+  //   return null;
+  // }
+
+  forbiddenProjectNameValidatorAsync(
+    control: FormControl
+  ): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (
+          // because of this we need to bind, else it will not get access to this.invalidProjectName
+          control.value.toLowerCase() === this.invalidProjectName.toLowerCase()
+        ) {
+          resolve({ projectNameIsForbidden: true });
+        } else {
+          resolve(null);
+        }
+      }, 1200);
+    });
+
+    return promise;
+  }
+
+  onSubmit() {
+    console.log(this.projectForm);
+  }
 }
